02b923af867e3d7566e4027ead005879aefb8f7f
wilsonge <georgejameswilson@googlemail.com>
2015-06-13 19:23:23 +0100

    Move layouts to root. Add service providers

--- src/Application.php
+++ src/Application.php
@@ -36,21 +36,15 @@ class Application extends AbstractWebApplication implements ContainerAwareInterf
 	 * @param   Input      $input      An optional argument to provide dependency injection for the application's
 	 *                                 input object.  If the argument is a Input object that object will become
 	 *                                 the application's input object, otherwise a default input object is created.
-	 * @param   Registry   $config     An optional argument to provide dependency injection for the application's
-	 *                                 config object.  If the argument is a Registry object that object will become
-	 *                                 the application's config object, otherwise a default config object is created.
 	 * @param   WebClient  $client     An optional argument to provide dependency injection for the application's
 	 *                                 client object.  If the argument is a Web\WebClient object that object will become
 	 *                                 the application's client object, otherwise a default client object is created.
 	 *
 	 * @since   1.0
 	 */
-	public function __construct(Container $container, Input $input = null, Registry $config = null, WebClient $client = null)
+	public function __construct(Container $container, Input $input = null, WebClient $client = null)
 	{
-		if ($config === null)
-		{
-			$config = $this->initialiseConfig();
-		}
+		$config = $container->get('config');
 
 		parent::__construct($input, $config, $client);
 
@@ -65,31 +59,6 @@ class Application extends AbstractWebApplication implements ContainerAwareInterf
 	}
 
 	/**
-	 * Initialises Webservices configuration
-	 *
-	 * @return  Registry|null
-	 */
-	public function initialiseConfig()
-	{
-		$plugin = \JPluginHelper::getPlugin('system', 'webservices');
-		$config = null;
-
-		if ($plugin)
-		{
-			if (is_string($plugin->params))
-			{
-				$config = new Registry($plugin->params);
-			}
-			elseif (is_object($plugin->params))
-			{
-				$config = $plugin->params;
-			}
-		}
-
-		return $config;
-	}
-
-	/**
 	 * Method to run the application routines.  Most likely you will want to instantiate a controller
 	 * and execute it, or perform some sort of task directly.
 	 *
@@ -99,8 +68,6 @@ class Application extends AbstractWebApplication implements ContainerAwareInterf
 	 */
 	public function doExecute()
 	{
-		// Load library language
-		$lang = \JFactory::getLanguage();
-		$lang->load('lib_webservices', JPATH_SITE);
+		$this->getContainer()->get('Joomla\\Language\\LanguageFactory')->getLanguage()->load('lib_webservices');
 	}
 }
