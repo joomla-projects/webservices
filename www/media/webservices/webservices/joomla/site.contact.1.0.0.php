623813925b0571e1d907b87f7dbf681dc42ff52b
George Wilson <georgejameswilson@googlemail.com>
2015-06-21 16:42:59 +0100

    Fix constant check when including helper

--- www/media/webservices/webservices/joomla/site.contact.1.0.0.php
+++ www/media/webservices/webservices/joomla/site.contact.1.0.0.php
@@ -7,7 +7,7 @@
  * @license     GNU General Public License version 2 or later; see LICENSE.txt
  */
 
-defined('JPATH_BASE') or die;
+defined('JPATH_API') or die;
 
 /**
  * Api Helper class for overriding default methods
