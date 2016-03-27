79f0b74221c9e91adcfc74a13903a6485cf11ac3
Matias Aguirre <maguirre@matware.com.ar>
2016-03-27 09:42:39 +0200

    Stylish

--- src/Service/WebconfigProvider.php
+++ src/Service/WebconfigProvider.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Configuration Service Provider for Joomla Webservices.
- *
- * @package    Webservices
- * @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
- * @license    GNU General Public License version 2 or later; see LICENSE.txt
- */
+* Configuration Service Provider for Joomla Webservices.
+*
+* @package    Webservices
+* @copyright  Copyright (C) 2005 - 2015 Open Source Matters, Inc. All rights reserved.
+* @license    GNU General Public License version 2 or later; see LICENSE.txt
+*/
 
 namespace Joomla\Webservices\Service;
 
@@ -14,26 +14,26 @@ use Joomla\DI\ServiceProviderInterface;
 use Joomla\Registry\Registry;
 
 /**
- * Configuration service provider
- *
- * @since  1.0
- */
+* Configuration service provider
+*
+* @since  1.0
+*/
 class WebconfigProvider implements ServiceProviderInterface
 {
 	/**
-	 * Configuration instance
-	 *
-	 * @var    Registry
-	 * @since  1.0
-	 */
+	* Configuration instance
+	*
+	* @var    Registry
+	* @since  1.0
+	*/
 	private $config;
 
 	/**
-	 * Constructor.
-	 *
-	 * @since   1.0
-	 * @throws  \RuntimeException
-	 */
+	* Constructor.
+	*
+	* @since   1.0
+	* @throws  \RuntimeException
+	*/
 	public function __construct()
 	{
 		// Set the configuration file path for the application.
@@ -53,29 +53,29 @@ class WebconfigProvider implements ServiceProviderInterface
 			throw new \RuntimeException(sprintf('Unable to parse the configuration file %s.', $file));
 		}
 
-    // Get the Joomla! configuration parameters
-  	$config = new \JConfig();
+		// Get the Joomla! configuration parameters
+		$config = new \JConfig();
 
-    // Set the correct database values for config object
-    $configObject->database->driver = $config->dbtype;
-    $configObject->database->host = $config->host;
-    $configObject->database->user = $config->user;
-    $configObject->database->password = $config->password;
-    $configObject->database->prefix = $config->dbprefix;
+		// Set the correct database values for config object
+		$configObject->database->driver = $config->dbtype;
+		$configObject->database->host = $config->host;
+		$configObject->database->user = $config->user;
+		$configObject->database->password = $config->password;
+		$configObject->database->prefix = $config->dbprefix;
 
 		$this->config = (new Registry)->loadObject($configObject);
 		$this->config->set('language.basedir', JPATH_API . '/src');
 	}
 
 	/**
-	 * Registers the service provider with a DI container.
-	 *
-	 * @param   Container  $container  The DI container.
-	 *
-	 * @return  void
-	 *
-	 * @since   1.0
-	 */
+	* Registers the service provider with a DI container.
+	*
+	* @param   Container  $container  The DI container.
+	*
+	* @return  void
+	*
+	* @since   1.0
+	*/
 	public function register(Container $container)
 	{
 		$container->set('config',
